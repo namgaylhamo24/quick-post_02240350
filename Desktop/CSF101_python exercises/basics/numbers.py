5ed8557910ae9d7c2f3144a931c4f67d2a63c529
namgaylhamo24 <02240350.cst@rub.edu.bt>
2024-08-31 08:42:52 +0600

    basics

---
+++ Desktop/CSF101_python exercises/basics/numbers.py
@@ -0,0 +1,11 @@
+age = 21  
+print(age)
+
+height = 1.68  
+print(height)
+
+age_in_days = age * 365
+print(age_in_days)
+
+result = age / 7
+print(result)
---
+++ Desktop/CSF101_python exercises/basics/strings.py
@@ -0,0 +1,12 @@
+name = "Namgay Lhamo"  
+print(name)
+
+greeting = "Hello, " + name + "!"
+print(greeting)
+
+greeting_f = f"Hello, {name}!"
+print(greeting_f)
+
+name_length = len(name)
+print(name_length)
+
